643a9863580f47289853b426de99d9669bbbb6bd
kim <kim@kim-PC>
2014-09-27 15:37:33 +0900

    2014-09-27

---
+++ Spring3.2_AOP_Example/src/main/java/com/meteor/aop/Basic_aop.java
@@ -0,0 +1,49 @@
+package com.meteor.aop;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class Basic_aop {
+	
+	@Pointcut("execution(public * com.meteor.controller..*(..))")
+	public void point_cut_area(){
+		
+	}
+
+	@Around( value="point_cut_area()")
+	//@Around(value="point_cut_area()")
+	public Object around(ProceedingJoinPoint joinpoint) throws Throwable{
+		
+		Object result = null;
+		
+		System.out.println("before 효과");
+
+		HttpServletRequest request = null;
+        HttpServletResponse response = null;
+        
+        for ( Object o : joinpoint.getArgs() ){ 
+            if ( o instanceof HttpServletRequest ) {
+                request = (HttpServletRequest)o;
+                System.out.println( request );
+            } 
+            if ( o instanceof HttpServletResponse ) {
+                response = (HttpServletResponse)o;
+            } 
+        }
+		result = joinpoint.proceed();
+	
+		System.out.println("after 효과");
+		
+		return result;
+		
+	}
+	
+}
